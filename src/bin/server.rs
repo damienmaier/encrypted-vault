e56fd105da1aefb419558521f053cb99e506413c
damien <91182145+damienmaier@users.noreply.github.com>
2023-01-16 04:06:44 +0100

    fix server

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,8 +1,10 @@
+use std::path::PathBuf;
 use vault::server::http_server;
 use vault::server::server_config::ServerConfig;
 
 fn main() {
-    println!("Hello, server!");
+    let server_port = ServerConfig::get().server_port;
 
-    http_server::run_http_server(ServerConfig::get().server_port, "vault-data".into());
+    println!("Server listening on port {server_port}");
+    http_server::run_http_server(server_port, PathBuf::from("vault-data"));
 }
